1d0ea3ad30503e3980aea9dba6a526894d329805
Booxxyy <andrent20@mail.ru>
2026-02-18 23:17:35 +0500

    Fix db

--- access_control/admin.py
+++ access_control/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Role, BusinessElement, AccessRoleRule
 
 # Register your models here.
+from django.contrib import admin
+from .models import Role, BusinessElement, AccessRoleRule
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(BusinessElement)
+class BusinessElementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(AccessRoleRule)
+class AccessRoleRuleAdmin(admin.ModelAdmin):
+    list_display = (
+        'role', 'element',
+        'read_permission', 'read_all_permission',
+        'create_permission',
+        'update_permission', 'update_all_permission',
+        'delete_permission', 'delete_all_permission',
+    )
+    list_filter = ('role', 'element')
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'access_control',
+    'users',
+    'mock_app',
 ]
 
 MIDDLEWARE = [
